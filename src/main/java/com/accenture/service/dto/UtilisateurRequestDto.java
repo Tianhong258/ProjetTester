b6c936ae0c778ddb3e324239a73054a62da3de30
tianhong.huang <tianhong.huang@accenture.com>
2025-02-19 14:36:53 +0100

    validation utilisateur commencée

--- src/main/java/com/accenture/service/dto/UtilisateurRequestDto.java
+++ src/main/java/com/accenture/service/dto/UtilisateurRequestDto.java
@@ -1,10 +1,14 @@
 package com.accenture.service.dto;
 
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UtilisateurRequestDto(
 
    String login,
+
+   @NotNull(message = "Le mot de passe est obligatoire")
    String password,
    String nom,
    String prenom
